586bcb7f2ec0091a6a1bde6b5191ac455145e716
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-08-05 13:05:35 -0400

    Add sed to strip trailing the new line

--- .github/workflows/sort_csv.sh
+++ .github/workflows/sort_csv.sh
@@ -8,15 +8,16 @@ HEADER=$(head -n 1 $FILENAME)
 # Extract content and sort it
 tail -n +2 $FILENAME | sort > $SORTED_FILENAME
 # Add header back
-{
-  echo "$HEADER"
-  cat $SORTED_FILENAME
-} > temp && mv temp $SORTED_FILENAME
+echo "$HEADER" | cat - $SORTED_FILENAME > temp && mv temp $SORTED_FILENAME
 
 # Check if sorted file is different from original
 if ! cmp --silent $FILENAME $SORTED_FILENAME; then
   # Replace original with sorted file
   mv $SORTED_FILENAME $FILENAME
+
+  # Remove the newline at the end of the file, if it exists
+  sed -i.bak -e '${/^$/d;}' $FILENAME && rm $FILENAME.bak
+
   echo "CSV file has been sorted."
   git config user.name "Automated"
   git config user.email "actions@users.noreply.github.com"
